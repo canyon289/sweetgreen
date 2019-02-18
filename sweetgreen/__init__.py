32d4102198c310d2b3079274cd29d422e8ddf25c
Ravin Kumar <ravinsdrive@gmail.com>
2019-02-17 21:01:31 -0800

    Add logging

--- sweetgreen/__init__.py
+++ sweetgreen/__init__.py
@@ -1,3 +1,29 @@
 """
 sweetgreen analysis repository
 """
+
+import logging
+import time
+
+
+def getLogger():
+    logger = logging.getLogger()
+    logger.setLevel(level=logging.NOTSET)
+
+    file_handler = logging.FileHandler("SweetGreen.log", encoding="utf-8")
+
+    formatter = logging.Formatter('%(asctime)s - %(levelname)-8s %(name)s - %(message)s',
+                                  '%Y-%m-%d %H:%M:%S UTC')
+    formatter.converter = time.gmtime
+
+    file_handler.setFormatter(formatter)
+    file_handler.converter = time.gmtime
+
+    # Also log to console
+    stream_handler = logging.StreamHandler()
+    stream_handler.setFormatter(formatter)
+
+    logger.addHandler(file_handler)
+    logger.addHandler(stream_handler)
+
+    return logger
