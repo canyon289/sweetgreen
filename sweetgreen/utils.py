7a700c4a00c04588ea72a9b6a50de7ce05a10733
Ravin Kumar <ravinsdrive@gmail.com>
2019-02-18 16:32:29 -0800

    Add utility method for loading directory of JSON files

--- sweetgreen/utils.py
+++ sweetgreen/utils.py
@@ -3,6 +3,7 @@ Utility Functions
 """
 import json
 import os
+import glob
 
 
 def write_json(file_path, file_name, obj):
@@ -13,7 +14,20 @@ def write_json(file_path, file_name, obj):
     return
 
 
-def read_json(file_name, file_path):
-    full_file_path = os.path.join(file_path, file_name)
-    with open(full_file_path, "r") as file_input:
+def read_json(file_name, file_path=None):
+    """Read file from json. Specify file_path optionally"""
+    if file_path:
+        file_name = os.path.join(file_path, file_name)
+
+    with open(file_name, "r") as file_input:
         return json.load(file_input)
+
+
+def load_all_restaurant_menus(file_path):
+    """Loads all json files from one directory into an array"""
+    glob_string = "{}/{}".format(file_path, "*.json")
+    json_paths = glob.iglob(glob_string)
+
+    jsons = [read_json(json_path) for json_path in json_paths]
+
+    return jsons
